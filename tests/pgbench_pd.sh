a0b5e45038a2c0001168c4c5e1d2948aeab752d2
Oliver Yuan <yuan.zg@91t.com>
2023-09-18 06:45:25 +0800

    [ADD] Add initial files

--- tests/pgbench_pd.sh
+++ tests/pgbench_pd.sh
@@ -3,10 +3,10 @@ export PORT='5433'
 export THREADS="10"
 export CLIENTS="10 30 50 100 300 500"
 
-#./scripts/pgbench_0.sh
-#./scripts/pgbench_1.sh > ./data/pgbench_pd_1.txt
-#./scripts/pgbench_2.sh > ./data/pgbench_pd_2.txt
-#./scripts/pgbench_3.sh > ./data/pgbench_pd_3.txt
-#./scripts/pgbench_4.sh > ./data/pgbench_pd_4.txt
-#./scripts/pgbench_5.sh > ./data/pgbench_pd_5.txt
+./scripts/pgbench_0.sh
+./scripts/pgbench_1.sh > ./data/pgbench_pd_1.txt
+./scripts/pgbench_2.sh > ./data/pgbench_pd_2.txt
+./scripts/pgbench_3.sh > ./data/pgbench_pd_3.txt
+./scripts/pgbench_4.sh > ./data/pgbench_pd_4.txt
+./scripts/pgbench_5.sh > ./data/pgbench_pd_5.txt
 ./scripts/pgbench_6.sh > ./data/pgbench_pd_6.txt
